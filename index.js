2b0b4d5d3d2e29993ea1bdfdf78c786404c0b489
arjun51001 <sainatharjun4@gmail.com>
2022-04-16 01:26:53 +0530

    commit 1

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const cors=require('cors')
+const nodeCron = require("node-cron");
+let updateFun=require('./modeler/functions')
+
+app.use(express.static(__dirname+"/public"));
+app.use(cors())
+
+let errorHandler=function(error){console.log(error)}
+
+
+app.post('/washProcess/:orderID/:totalItems', function(req,res){
+  updateFun.washingFun(req.params.orderID,req.params.totalItems,res);
+})
+
+app.post('/ironProcess/:orderID/:totalItems', function(req,res){
+  updateFun.ironingFun(req.params.orderID,req.params.totalItems,res);
+})
+
+const job = nodeCron.schedule("0 0 0 * * *", function jobYouNeedToExecute() {
+  // Do whatever you want in here. Send email, Make  database backup or download data.
+  updateFun.resetFun();
+});
+
+
+app.listen(process.env.PORT || 5000,function(error){
+  if(error){
+    console.log(error)
+  }
+  else{
+    job;
+    console.log('Listening at 5000')
+  }
+})
---
+++ modeler/functions.js
@@ -0,0 +1,180 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db=admin.firestore()
+
+
+  const updateStatusFun=async (orderID,status)=>{
+    var docRef = db.collection("Orders");
+    var updateDoc=""
+    var docid=""
+            // Set the "capital" field of the city 'DC'
+            await docRef.where('orderID','==',orderID).get().then((s)=>{
+              s.forEach(doc=>{
+                docRef.doc(doc.id).update({
+                  status:status
+              })
+              .then(() => {
+                  console.log("Updated ",status);
+              })
+              .catch((error) => {
+                  // The document probably doesn't exist.
+                  console.error("Error updating document: ", error);
+              });
+              })
+
+          
+            })
+            
+             
+  }
+
+  const updateToPreparingToWash = (orderID) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            status = 'Preparing to wash';
+            updateStatusFun(orderID,status)
+            resolve(true);
+        }, 5000);
+    });
+};
+
+const updateToPreparingToIron = (orderID) => {
+  return new Promise((resolve, reject) => {
+      setTimeout(()=>{
+          status = 'Preparing to iron';
+          updateStatusFun(orderID,status)
+          resolve(true);
+      }, 5000);
+  });
+};
+
+const updateToWashing = (orderID) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            status = 'Washing';
+            updateStatusFun(orderID,status)
+            resolve(true);
+        }, 5000);
+    });
+};
+
+const updateToDrying = (orderID) => {
+  return new Promise((resolve, reject) => {
+      setTimeout(()=>{
+          status = 'Drying';
+          updateStatusFun(orderID,status)
+          resolve(true);
+      }, 5000);
+  });
+};
+
+const updateToIroning = async (orderID,totalItems) => {
+  let ret;
+  await db.collection('orderCount').doc('orderCountPerDay').get().then(async (res)=>{
+    if(res.data().Ironing+parseInt(totalItems)<100){
+      await db.collection('orderCount').doc('orderCountPerDay').update({Ironing:parseInt(res.data().Ironing)+parseInt(totalItems)}).then(()=>{
+        ret= new Promise((resolve, reject) => {
+          setTimeout(()=>{
+              status = 'Ironing';
+              updateStatusFun(orderID,status)
+              resolve(true);
+          }, 5000);
+      });
+      
+      })
+
+    }
+  else{
+    ret="100 clothes reached"
+  }
+  })
+  return ret;
+};
+const readyToPickUp = (orderID) => {
+  return new Promise((resolve, reject) => {
+      setTimeout(()=>{
+          status = 'Ready to pick up';
+          updateStatusFun(orderID,status)
+          resolve(true);
+      }, 5000);
+  });
+};
+
+const washProcess = async (orderID,totalItems) => {
+  db.collection('orderCount').doc('orderCountPerDay').get().then((res)=>{
+    if(res.data().Washing+parseInt(totalItems)<100){
+      db.collection('orderCount').doc('orderCountPerDay').update({Washing:parseInt(res.data().Washing)+parseInt(totalItems)}).then(()=>{
+        updateToPreparingToWash(orderID).then(()=>{
+          updateToWashing(orderID).then(()=>{
+            updateToDrying(orderID).then(()=>{
+              updateToIroning(orderID,totalItems).then((res)=>{
+                if(res=='100 clothes reached'){
+                  return res
+                }
+                readyToPickUp(orderID).then(()=>{
+                  console.log('Washing process finished')
+                })
+              })
+            })
+          })
+         });
+      })
+      return 'Pushed for washing'
+    }
+    else{
+      return '100 clothes reached'
+    }
+  })
+     
+}
+
+const ironProcess = async (orderID,totalItems) => {
+  db.collection('orderCount').doc('orderCountPerDay').get().then((res)=>{
+    if(res.Ironing+parseInt(totalItems)<100){
+      db.collection('orderCount').doc('orderCountPerDay').update({Ironing:parseInt(res.Ironing)+parseInt(totalItems)}).then(()=>{
+        updateToPreparingToIron(orderID).then(()=>{
+              updateToIroning(orderID).then(()=>{
+                readyToPickUp(orderID).then(()=>{
+                  console.log('Ironing process finished')
+                })
+            })
+         });
+      })
+      return 'Pushed for ironing'
+    }
+    else{
+      return '100 clothes reached'
+    }
+  })
+     
+}
+
+
+module.exports.washingFun = (orderID,totalItems,res)=>{
+    let ret=washProcess(orderID,totalItems)
+    res.send(ret)
+  
+}
+
+module.exports.ironingFun = (orderID,totalItems,res)=>{
+
+  let ret=ironProcess(orderID,totalItems)
+  res.send(ret)
+
+}
+
+module.exports.resetFun = ()=>{
+
+  db.collection('orderCount').doc('orderCountPerDay').update({
+    Washing:0,
+    Ironing:0
+  })
+
+}
